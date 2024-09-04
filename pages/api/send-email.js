605805c78a079f5f1e8c3f5466210976123bb324
raho-droid <rahmanalim36@gmail.com>
2024-09-04 04:55:34 +0300

    fixed

---
+++ pages/api/send-email.js
@@ -0,0 +1,32 @@
+import nodemailer from "nodemailer";
+
+export default async function handler(req, res) {
+  if (req.method === "POST") {
+    const { name, email, subject, message } = req.body;
+
+    const transporter = nodemailer.createTransport({
+      service: "gmail",
+      auth: {
+        user: process.env.EMAIL_USER,
+        pass: process.env.EMAIL_PASS,
+      },
+    });
+
+    const mailOptions = {
+      from: email,
+      to: "rahmanalim36@gmail.com",
+      subject: subject,
+      text: `Name: ${name}\nEmail: ${email}\nMessage: ${message}`,
+    };
+
+    try {
+      await transporter.sendMail(mailOptions);
+      res.status(200).json({ message: "Email sent successfully!" });
+    } catch (error) {
+      console.error("Error sending email:", error);
+      res.status(500).json({ error: "Failed to send email." });
+    }
+  } else {
+    res.status(405).json({ message: "Method not allowed" });
+  }
+}
--- pages/work/index.js
+++ pages/work/index.js
@@ -1,48 +1,49 @@
 // components
-import WorkSlider from '../../components/WorkSlider';
-import Bulb from '../../components/Bulb';
-import Circles from '../../components/Circles';
+import WorkSlider from "../../components/WorkSlider";
+import Bulb from "../../components/Bulb";
+import Circles from "../../components/Circles";
 
 // framer motion
-import { motion } from 'framer-motion';
-import { fadeIn } from '../../variants';
+import { motion } from "framer-motion";
+import { fadeIn } from "../../variants";
 
 const Work = () => {
   return (
-    <div className='h-full bg-primary/30 py-36 flex items-center'>
+    <div className="h-full bg-primary/30 py-36 flex items-center">
       <Circles />
-      <div className='container mx-auto'>
-        <div className='flex flex-col xl:flex-row gap-x-8'>
+      <div className="container mx-auto">
+        <div className="flex flex-col xl:flex-row gap-x-8">
           {/* text */}
-          <div className='text-center flex xl:w-[30vw] flex-col lg:text-left mb-4 xl:mb-0'>
+          <div className="text-center flex xl:w-[30vw] flex-col lg:text-left mb-4 xl:mb-0">
             <motion.h2
-              variants={fadeIn('up', 0.2)}
-              initial='hidden'
-              animate='show'
-              exit='hidden'
-              className='h2 xl:mt-12'
+              variants={fadeIn("up", 0.2)}
+              initial="hidden"
+              animate="show"
+              exit="hidden"
+              className="h2 xl:mt-12"
             >
-              My work <span className='text-accent'>.</span>
+              My Works <span className="text-accent">.</span>
             </motion.h2>
             <motion.p
-              variants={fadeIn('up', 0.4)}
-              initial='hidden'
-              animate='show'
-              exit='hidden'
-              className='mb-4 max-w-[400px] mx-auto lg:mx-0'
+              variants={fadeIn("up", 0.4)}
+              initial="hidden"
+              animate="show"
+              exit="hidden"
+              className="mb-4 max-w-[400px] mx-auto lg:mx-0"
             >
-              Lorem ipsum dolor sit, amet consectetur adipisicing elit. A
-              eveniet dignissimos distinctio tempore harum laudantium.
+              Most of the projects I've worked on are secured applications that
+              require login, so I can only share their screenshots. If you'd
+              like to see a live preview, feel free to contact me.
             </motion.p>
           </div>
 
           {/* slider */}
           <motion.div
-            variants={fadeIn('down', 0.6)}
-            initial='hidden'
-            animate='show'
-            exit='hidden'
-            className='w-full xl:max-w-[65%]'
+            variants={fadeIn("down", 0.6)}
+            initial="hidden"
+            animate="show"
+            exit="hidden"
+            className="w-full xl:max-w-[65%]"
           >
             <WorkSlider />
           </motion.div>
